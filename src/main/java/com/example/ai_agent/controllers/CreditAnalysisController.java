34de49f1bd37cad4feb4da36624ad251cdc2dc43
Cristiano Andrade de Aguiar <cristianoandrade.deaguiar@gmail.com>
2026-07-09 21:29:19 -0300

    Ajustando contrato

--- src/main/java/com/example/ai_agent/controllers/CreditAnalysisController.java
+++ src/main/java/com/example/ai_agent/controllers/CreditAnalysisController.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/chat")
+@RequestMapping("/api/chat")
 public class CreditAnalysisController {
 
     private static final Logger log = LoggerFactory.getLogger(CreditAnalysisController.class);
